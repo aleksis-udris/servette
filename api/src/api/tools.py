e928c67b8c61fa758bc83f636046184b0ff8ac47
aleksis-udris <aleksis060806@gmail.com>
2026-09-16 13:37:24 +0300

    Added classes in preperation for post endpoints

--- api/src/api/tools.py
+++ api/src/api/tools.py
@@ -1,8 +1,91 @@
 from os import getenv
 import asyncpg
 from dotenv import load_dotenv
+from pydantic import BaseModel, EmailStr
+from enum import Enum
+from datetime import datetime, date
 
 load_dotenv()
+
+# Done
+class Visibility(str, Enum):
+    public = "public"
+    private = "private"
+    priviledged = "priviledged"
+
+# Done
+class PlanTier(str, Enum):
+    basic = "basic"
+    premium = "premium"
+
+# Done
+class DeviceType(str, Enum):
+    mobile = "mobile"
+    computer = "computer"
+    tv = "tv"
+
+# Done
+class UserOnLogin(BaseModel):
+    username: str
+    password: str
+
+# Done
+class UserOnRegister(UserOnLogin):
+    name: str
+    surname: str
+    email: EmailStr
+    confirmation_password: str
+    date_of_birth: date
+    country: str
+
+# Done
+class UserOnUpdate(BaseModel):
+    name: str | None = None
+    surname: str | None = None
+    username: str | None = None
+    email: EmailStr | None = None
+    date_of_birth: date | None = None
+    country: str | None = None
+
+# Done
+class PlaylistOnCreate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    playlist_visibilty: Visibility
+
+# Done
+class PlaylistOnUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    playlist_visibility: Visibility | None = None
+
+# Done
+class PlaylistSongAdd(BaseModel):
+    song_id: str
+    user_id: str
+    position: int
+
+# Done
+class AllowedEditor(BaseModel):
+    user_id: str
+    allower_id: str
+
+# Done
+class DeviceRegistration(BaseModel):
+    device_type: DeviceType
+
+# Done
+class UserHistorySong(BaseModel):
+    song_id: str
+    device_id: str
+    file_id: str
+    listened_ms: int
+    skipped_at: int
+
+# Done
+class SuggestedSong(BaseModel):
+    friend_id: str
+    song_id: str
 
 async def open_connection():
         try:
